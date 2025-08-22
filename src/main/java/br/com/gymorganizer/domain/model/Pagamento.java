25666d1dc2957f19b4a4ed4bf4fafa9a9573598c
raellrrm <gamingprodf@gmail.com>
2025-08-22 10:27:57 -0300

    feat: correção do atributo dataPagamento

--- src/main/java/br/com/gymorganizer/domain/model/Pagamento.java
+++ src/main/java/br/com/gymorganizer/domain/model/Pagamento.java
@@ -3,6 +3,7 @@ package br.com.gymorganizer.domain.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -17,6 +18,7 @@ public class Pagamento {
     @Id
     private Long id;
 
+    @CreationTimestamp
     @Column(name = "data_pagamento", nullable = false)
     private LocalDateTime dataPagamento;
 
