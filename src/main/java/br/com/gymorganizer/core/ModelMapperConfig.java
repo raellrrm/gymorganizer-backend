fb7f8a62b72bf3d9b2d08a87772d16ffd4be1372
raellrrm <gamingprodf@gmail.com>
2025-08-29 18:01:34 -0300

    feat: configurar ModelMapper

---
+++ src/main/java/br/com/gymorganizer/core/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package br.com.gymorganizer.core;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.modelmapper.ModelMapper;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
