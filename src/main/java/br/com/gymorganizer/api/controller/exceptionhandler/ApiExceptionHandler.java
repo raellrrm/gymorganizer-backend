809aff917ca9245c1d4f1d4dc686a06b946ef822
raellrrm <gamingprodf@gmail.com>
2025-08-29 14:25:20 -0300

    refactor: tratar excecões não mapeadas e retornar demais exceptions padronizadas

--- src/main/java/br/com/gymorganizer/api/controller/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/gymorganizer/api/controller/exceptionhandler/ApiExceptionHandler.java
@@ -1,7 +1,9 @@
 package br.com.gymorganizer.api.controller.exceptionhandler;
 
 import br.com.gymorganizer.domain.exception.*;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,67 +13,86 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import java.time.LocalDateTime;
 
 /**
- * Classe responsável por capturar e tratar exceções da aplicação.
- *
- * Ocorre ao tentar realizar um pagamento quando o usuário já está com o status de pagamento ativo
+ * Captura e trata exceções da aplicação, retornando respostas padronizadas.
  */
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+    /**
+     * 400 - Usuário já está ativo.
+     */
     @ExceptionHandler(UsuarioJaAtivoException.class)
     public ResponseEntity<?> handleUsuarioJaAtivoException(UsuarioJaAtivoException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.BAD_REQUEST; // 400
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         ProblemType type = ProblemType.USUARIO_JA_ATIVO;
 
         Problem problem = createProblemBuilder(type, ex.getMessage(), status, LocalDateTime.now()).build();
-        return ResponseEntity.status(status).body(problem);
+        return handleExceptionInternal(ex, problem,new HttpHeaders(), status, request);
     }
 
     /**
-     * Trata a exceção AtributoEmUsoException.
-     *
-     * Ocorre quando algum atributo que deveria ser único já está em uso.
+     * 400 - Atributo único já em uso.
      */
     @ExceptionHandler(AtributoEmUsoException.class)
     public ResponseEntity<?> handleAtributoEmUso(AtributoEmUsoException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.BAD_REQUEST; // 400
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         ProblemType type = ProblemType.ATRIBUTO_EM_USO;
 
         Problem problem = createProblemBuilder(type, ex.getMessage(), status, LocalDateTime.now()).build();
-        return ResponseEntity.status(status).body(problem);
+        return handleExceptionInternal(ex, problem,new HttpHeaders(), status, request);
     }
 
     /**
-     * Trata a exceção EntidadeEmUsoException.
-     *
-     * Ocorre quando uma entidade não pode ser removida/alterada
-     * por estar vinculada a outra (ex.: chave estrangeira).
+     * 400 - Entidade vinculada a outra (em uso).
      */
     @ExceptionHandler(EntidadeEmUsoException.class)
     public ResponseEntity<?> handleEntidadeEmUso(EntidadeEmUsoException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.BAD_REQUEST; // 400
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         ProblemType type = ProblemType.ENTIDADE_EM_USO;
 
         Problem problem = createProblemBuilder(type, ex.getMessage(), status, LocalDateTime.now()).build();
-        return ResponseEntity.status(status).body(problem);
+        return handleExceptionInternal(ex, problem,new HttpHeaders(), status, request);
     }
 
     /**
-     * Trata a exceção EntidadeNaoEncontradaException.
-     *
-     * Ocorre quando uma entidade buscada por ID não é encontrada no banco.
+     * 404 - Entidade não encontrada.
      */
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> handleEntidadeNaoEncontrada(EntidadeNaoEncontradaException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.NOT_FOUND; // 404
+        HttpStatus status = HttpStatus.NOT_FOUND;
         ProblemType type = ProblemType.RECURSO_NAO_ENCONTRADO;
 
         Problem problem = createProblemBuilder(type, ex.getMessage(), status, LocalDateTime.now()).build();
-        return ResponseEntity.status(status).body(problem);
+        return handleExceptionInternal(ex, problem,new HttpHeaders(), status, request);
     }
 
     /**
-     * Método auxiliar para construir objetos do tipo Problem (corpo do erro).
+     * Trata exceções não mapeadas ou sem corpo definido.
+     * Retorna erro padronizado no formato {@link Problem}.
+     */
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex,
+            Object body,
+            HttpHeaders headers,
+            HttpStatusCode statusCode,
+            WebRequest request
+    ) {
+        if(body == null) {
+            HttpStatus status = HttpStatus.valueOf(statusCode.value());
+            body = Problem.builder()
+                    .status(statusCode.value())
+                    .title(status.getReasonPhrase())
+                    .detail(ex.getMessage())
+                    .timestamp(LocalDateTime.now())
+                    .build();
+        }
+
+        return new ResponseEntity<>(body, headers, statusCode);
+    }
+
+    /**
+     * Constrói um {@link Problem} com dados consistentes.
      */
     public Problem.ProblemBuilder createProblemBuilder(
             ProblemType type, String detail, HttpStatus status, LocalDateTime timestamp) {
