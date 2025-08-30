19eec5d53b30182f341728b4202bdf505f8967b8
raellrrm <gamingprodf@gmail.com>
2025-08-29 21:40:06 -0300

    criar dto-usuario de input

---
+++ src/main/java/br/com/gymorganizer/api/controller/model/usuario/PlanoIdInput.java
@@ -0,0 +1,11 @@
+package br.com.gymorganizer.api.controller.model.usuario;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PlanoIdInput {
+
+    private Long id;
+}
---
+++ src/main/java/br/com/gymorganizer/api/controller/model/usuario/UsuarioInput.java
@@ -0,0 +1,20 @@
+package br.com.gymorganizer.api.controller.model.usuario;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+public class UsuarioInput {
+
+    private String nome;
+    private String sobrenome;
+    private LocalDate dataNascimento;
+    private String telefone;
+    private String email;
+    private String cpf;
+    private PlanoIdInput plano;
+
+}
