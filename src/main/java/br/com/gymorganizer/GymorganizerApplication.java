1a85c13ae51a266bd6258b9e023057ae14c245e8
raellrrm <gamingprodf@gmail.com>
2025-09-01 15:35:36 -0300

    Primeiro commit: projeto Spring Boot inicializado

---
+++ src/main/java/br/com/gymorganizer/GymorganizerApplication.java
@@ -0,0 +1,13 @@
+package br.com.gymorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymorganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymorganizerApplication.class, args);
+	}
+
+}
