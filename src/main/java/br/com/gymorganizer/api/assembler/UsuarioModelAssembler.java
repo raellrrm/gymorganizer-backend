ca839944fc7b0010723adc07104a63a05e4a45b7
raellrrm <gamingprodf@gmail.com>
2025-09-01 16:49:28 -0300

    feat: create User assembler and disassembler

---
+++ src/main/java/br/com/gymorganizer/api/assembler/UsuarioModelAssembler.java
@@ -0,0 +1,27 @@
+package br.com.gymorganizer.api.assembler;
+
+import br.com.gymorganizer.api.controller.model.usuario.UsuarioModel;
+import br.com.gymorganizer.domain.model.Usuario;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class UsuarioModelAssembler {
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    public UsuarioModel toModel(Usuario usuario) {
+        return modelMapper.map(usuario, UsuarioModel.class);
+    }
+
+    public List<UsuarioModel> toCollectModel(List<Usuario> usuarios) {
+        return usuarios.stream()
+                .map(usuario -> toModel(usuario))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/br/com/gymorganizer/api/assembler/UsuarioModelDisassembler.java
@@ -0,0 +1,25 @@
+package br.com.gymorganizer.api.assembler;
+
+import br.com.gymorganizer.api.controller.model.usuario.UsuarioInput;
+import br.com.gymorganizer.domain.model.Plano;
+import br.com.gymorganizer.domain.model.Usuario;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UsuarioModelDisassembler {
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    public Usuario toDomainObject(UsuarioInput usuarioInput) {
+        return modelMapper.map(usuarioInput, Usuario.class);
+    }
+
+    public void copyToDomainObject(UsuarioInput usuarioInput, Usuario usuario) {
+        usuario.setPlano(new Plano());
+
+        modelMapper.map(usuarioInput, usuario);
+    }
+}
