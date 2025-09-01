dced4bad6e61251f3828cc007cacaff1c66ce81b
raellrrm <gamingprodf@gmail.com>
2025-09-01 16:24:16 -0300

    feat: create Plan service

---
+++ src/main/java/br/com/gymorganizer/domain/service/CadastroPlanoService.java
@@ -0,0 +1,23 @@
+package br.com.gymorganizer.domain.service;
+
+import br.com.gymorganizer.domain.model.Plano;
+import br.com.gymorganizer.domain.model.Usuario;
+import br.com.gymorganizer.domain.repository.PlanoRepository;
+import br.com.gymorganizer.domain.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroPlanoService {
+
+    @Autowired
+    PlanoRepository planoRepository;
+
+    public Plano salvar(Plano plano) {
+        return planoRepository.save(plano);
+    }
+
+    public void excluir(Long planoId) {
+        planoRepository.deleteById(planoId);
+    }
+}
